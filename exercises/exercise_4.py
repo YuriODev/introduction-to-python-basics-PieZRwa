07474eda77af7eabf6abab630465f7f16770b2fa
Pierrot <128379590+PieZRwa@users.noreply.github.com>
2024-03-25 12:09:31 +0000

    Update exercise_4.py

--- exercises/exercise_4.py
+++ exercises/exercise_4.py
@@ -1,2 +1,9 @@
 # Exercise 4
+def solution1():
+  def ifSymmetric(n):
+    return int(str(n) == str(n)[::-1])
+  
+  print(ifSymmetric(2002))
+  print(ifSymmetric(1234))
+  print(ifSymmetric(1221))
 # Your solution comes here
