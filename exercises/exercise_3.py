8fbfe8b38af30213b5dc2007bbf5d6d9daeb7f00
Pierrot <128379590+PieZRwa@users.noreply.github.com>
2024-03-25 12:03:19 +0000

    Update exercise_3.py

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -1,2 +1,17 @@
 # Exercise 3
+def time(s):
+  h = (s//3600)%24
+  m = (s%3600)//60
+  s = s%60
+  return f"{h}:{'0' if m < 10 else ''}{m}:{'0' if s < 10 else ''}{s}"
+
+print(
+  time(3602)
+)
+print(
+  time(4556789)
+)
+print(
+  time(4568)
+)
 # Your solution comes here
